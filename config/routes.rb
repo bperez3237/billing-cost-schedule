de3aab2d0bb9fc2df1acd80817c330d386ba504c
brian <b.perez3237@gmail.com>
2022-12-16 16:34:08 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :cost_schedules, only: [:index, :show, :create, :update, :destroy]
-  resources :location_categories, only: [:index, :show, :create, :update, :destroy]
-  resources :costs, only: [:index, :show, :create, :update, :destroy]
-  resources :locations,  only: [:index, :show, :create, :update, :destroy]
-  resources :subcontractors,  only: [:index, :show, :create, :update, :destroy]
-  resources :activities,  only: [:index, :show, :create, :update, :destroy]
+  # resources :cost_schedules, only: [:index, :show, :create, :update, :destroy]
+  resources :location_categories, only: [:index, :show]
+  # resources :costs, only: [:index, :show, :create, :update, :destroy]
+  # resources :locations,  only: [:index, :show, :create, :update, :destroy]
+  resources :subcontractors,  only: [:index, :show]
+  # resources :activities,  only: [:index, :show, :create, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
