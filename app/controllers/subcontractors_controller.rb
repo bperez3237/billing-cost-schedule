127adaad6cff594bc65ba9490571e071cc4fb23e
brian <b.perez3237@gmail.com>
2022-12-16 16:33:12 -0500

    Create controller methods for Subcontractor and location tables

--- app/controllers/subcontractors_controller.rb
+++ app/controllers/subcontractors_controller.rb
@@ -1,2 +1,12 @@
 class SubcontractorsController < ApplicationController
+
+    def index 
+        subcontractors = Subcontractor.all
+        render json: subcontractors
+    end
+
+    def show
+        subcontractor = Subcontractor.find(params[:id])
+        render json: subcontractor
+    end
 end
